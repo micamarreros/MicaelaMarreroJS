d185491eea346b5d65c0484736a90ec56f93831c
Micaela Marrero <micamarrero@gmail.com>
2023-07-24 20:40:17 -0300

    Seguí trabajando en arrays

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
 let nombreIngresado = prompt("Ingresá tu nombre");
 let saludo = alert("Hola " + nombreIngresado + " bienvenid@ a mi web");
 
-/*Mi objetivo es crear un tarifario de diseño grafico en el que el cliente pueda ver precios y se realice el calculo del mismo dependiendo del tipo de cliente */
+// Mi objetivo es crear un tarifario de diseño grafico en el que el cliente pueda ver precios y se realice el calculo del mismo dependiendo del tipo de cliente
 
 let aclaracionPrecios = alert("Te invito a descubrir los diferentes precios de este tarifario");
 
 const productos1 = [
-    {nombre: "Logo", precio: 2400, categoriaProducto: "Branding"},
-    {nombre: "Identidad", precio: 6000, categoriaProducto: "Branding"},
-    {nombre: "Manual", precio: 5000, categoriaProducto: "Branding"},
-    {nombre: "Papelería", precio: 2500, categoriaProducto: "Branding"},
+    {nombre: "Logo", precio: 2400, categoriaProducto: "Branding", tipoCliente: "A"},
+    {nombre: "Identidad", precio: 6000, categoriaProducto: "Branding", tipoCliente: "A"},
+    {nombre: "Manual", precio: 5000, categoriaProducto: "Branding", tipoCliente: "A"},
+    {nombre: "Papelería", precio: 2500, categoriaProducto: "Branding", tipoCliente: "A"},
 ]
 
 const productos2 = [
-    {nombre: "Etiqueta", precio: 1200, categoriaProducto: "Packaging"},
-    {nombre: "Stickers", precio: 450, categoriaProducto: "Packaging"},
+    {nombre: "Etiqueta", precio: 1200, categoriaProducto: "Packaging", tipoCliente: "A"},
+    {nombre: "Stickers", precio: 450, categoriaProducto: "Packaging", tipoCliente: "A"},
 ]
 
 const productos3 = [
-    {nombre: "Folleto Díptico", precio: 1400, categoriaProducto: "Editorial"},
-    {nombre: "Folleto Tríptico", precio: 1800, categoriaProducto: "Editorial"},
-    {nombre: "Brochure", precio: 3000, categoriaProducto: "Editorial"},
-    {nombre: "Afiche", precio: 2300, categoriaProducto: "Editorial"},
-    {nombre: "Aviso", precio: 500, categoriaProducto: "Editorial"},
+    {nombre: "Folleto Díptico", precio: 1400, categoriaProducto: "Editorial", tipoCliente: "A"},
+    {nombre: "Folleto Tríptico", precio: 1800, categoriaProducto: "Editorial", tipoCliente: "A"},
+    {nombre: "Brochure", precio: 3000, categoriaProducto: "Editorial", tipoCliente: "A"},
+    {nombre: "Afiche", precio: 2300, categoriaProducto: "Editorial", tipoCliente: "A"},
+    {nombre: "Aviso", precio: 500, categoriaProducto: "Editorial", tipoCliente: "A"},
 ]
 
 const productos4 = [
-    {nombre: "Feed", precio: 5500, categoriaProducto: "Redes"},
-    {nombre: "Plantilla", precio: 1000, categoriaProducto: "Redes"},
+    {nombre: "Feed", precio: 5500, categoriaProducto: "Redes", tipoCliente: "A"},
+    {nombre: "Plantilla", precio: 1000, categoriaProducto: "Redes", tipoCliente: "A"},
 ]
 
 
@@ -64,9 +64,9 @@ switch(categoriaProducto) {
         break;
 }
 
-/* Calculo de precio de acuerdo al tipo de cliente.: tipo A o tipo B 
-Cliente A: particulares, emprendimientos o empresas unipersonales. 
-Cliente B: Empresas medianas, grandes u organismos estatales */
+// Calculo de precio de acuerdo al tipo de cliente.: tipo A o tipo B 
+// Cliente A: particulares, emprendimientos o empresas unipersonales. 
+// Cliente B: Empresas medianas, grandes u organismos estatales
 
 class Producto {
     constructor (nombre, precio, tipoCliente){
@@ -91,7 +91,7 @@ function crearProducto () {
     return producto
 }
 
-/*Ciclo para evitar agregar más de 10 productos al carrito */
+// Ciclo para evitar agregar más de 10 productos al carrito
 
 let productosAgregados = 0;
 while (x <11) {
