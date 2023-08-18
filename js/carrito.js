fff2aa3740fdd2740d67cbe8ca0234bf136e4f6a
Micaela Marrero <micamarrero@gmail.com>
2023-08-18 00:14:25 -0300

    Correcion de errores en carrito

--- js/carrito.js
+++ js/carrito.js
@@ -4,18 +4,10 @@ const contenedorCarrito = document.getElementById("contenedor-carrito");
 // Contenedor de cada producto del carrito
 const verProductos = (productos) => {
     productos.forEach(producto =>{
-        const tarjetaProducto2 = JSON.parse.document.createElement("div");
-        tarjetaProducto2.className = "tarjeta-producto2";
-        tarjetaProducto2.innerHTML = `
-                                    <img class="producto-img" src="${producto.img}" alt="${producto.nombre}">
-                                    <div class="descripcion-productos2">
-                                        <h3 class="producto-nombre">${producto.nombre}</h3>
-                                        <p>$ ${producto.precio}</p>
-                                        
-                                        <button>Eliminar Producto</button>
-                                    </div>
-                                    `;
-        contenedorProductos.appendChild(tarjetaProducto2);
+        const tarjetaCarrito = document.createElement("div");
+        tarjetaCarrito.className = "tarjeta-carrito";
+        
+        contenedorProductos.appendChild(tarjetaCarrito);
     })
     const btnComprar = document.querySelectorAll('.btn-compra');
     btnComprar.forEach(el => {
