be73f235e0ca23ca3bfe4ad4344d29e1f5c91299
Micaela Marrero <micamarrero@gmail.com>
2023-08-22 22:26:27 -0300

    Correciones en carrito, agrego imagenes productos

--- js/carrito.js
+++ js/carrito.js
@@ -10,14 +10,14 @@ productosEnCarrito.forEach(producto => {
     const div = document.createElement("div");
     div.classList.add("carrito-producto");
     div.innerHTML= `
-    <img class="carrito-producto-img" src="${productos.img}" alt="${productos.nombre}">
+    <img class="carrito-producto-img" src="${producto.img}" alt="${producto.nombre}">
     <div class="carrito-descripcion-productos">
-        <h5 class="carrito-producto-nombre">${productos.nombre}</h5>
-        <p>$ ${productos.precio}</p>
-        <p>${productos.cantidad}</p>
-        <p>Subtotal ${productos.precio * productos.cantidad}</p>
+        <h5 class="carrito-producto-nombre">${producto.nombre}</h5>
+        <p>$ ${producto.precio}</p>
+        <p>${producto.cantidad}</p>
+        <p>Subtotal ${producto.precio * producto.cantidad}</p>
    
-        <button id='${productos.id}' class="btn-eliminar">Eliminar del carrito</button>
+        <button id='${producto.id}' class="btn-eliminar">Eliminar del carrito</button>
     </div>
 `;
 productosEnCarrito.append(div);
--- js/productos.js
+++ js/productos.js
@@ -3,72 +3,84 @@ const productos = [
         id:"1", 
         img: "../media/05tienda/01logo.jpg",
         nombre: "Logo", 
-        precio: 2400
+        precio: 2400,
+        cantidad: 0
     },
     {
         id:"2", 
         img: "../media/05tienda/02identidad.jpg",
         nombre: "Identidad", 
-        precio: 6000
+        precio: 6000,
+        cantidad: 0
     },
     {
         id:"3", 
-        img: "../media/05tienda/02identidad.jpg",
+        img: "../media/05tienda/03manual.jpg",
         nombre: "Manual", 
-        precio: 5000
+        precio: 5000,
+        cantidad: 0
     },
     {
         id:"4", 
-        img: "../media/05tienda/02identidad.jpg",
+        img: "../media/05tienda/04papeleria.jpg",
         nombre: "Papelería", 
-        precio: 2500
+        precio: 2500,
+        cantidad: 0
     },
     {
         id:"5", 
-        img: "../media/05tienda/02identidad.jpg",
+        img: "../media/05tienda/05etiqueta.jpg",
         nombre: "Etiqueta", 
-        precio: 1200
+        precio: 1200,
+        cantidad: 0
     },
     {
         id:"6", 
-        img: "../media/05tienda/02identidad.jpg",
+        img: "../media/05tienda/06stickers.jpg",
         nombre: "Stickers", 
-        precio: 450
+        precio: 450,
+        cantidad: 0
     },
     {
         id:"7", 
-        img: "../media/05tienda/02identidad.jpg",
+        img: "../media/05tienda/07folletodi.jpg",
         nombre: "Folleto Díptico", 
-        precio: 1400
+        precio: 1400,
+        cantidad: 0
     },
     {
         id:"8", 
-        img: "../media/05tienda/02identidad.jpg",
+        img: "../media/05tienda/08folletotri.jpg",
         nombre: "Folleto Tríptico", 
-        precio: 1800
+        precio: 1800,
+        cantidad: 0
     },
     {
         id:"9", 
-        img: "../media/05tienda/02identidad.jpg",
+        img: "../media/05tienda/09afiche.jpg",
         nombre: "Afiche", 
-        precio: 2300
+        precio: 2300,
+        cantidad: 0
     },
     {
         id:"10", 
-        img: "../media/05tienda/02identidad.jpg",
+        img: "../media/05tienda/10aviso.jpg",
         nombre: "Aviso", 
-        precio: 500
+        precio: 500,
+        cantidad: 0
     },
     {
         id:"11", 
-        img: "../media/05tienda/02identidad.jpg",
+        img: "../media/05tienda/11feed.jpg",
         nombre: "Feed Redes", 
-        precio: 5500
+        precio: 5500,
+        cantidad: 0
     },
     {
         id:"12", 
-        img: "../media/05tienda/02identidad.jpg",
+        img: "../media/05tienda/12redes.jpg",
         nombre: "Plantilla Redes", 
-        precio: 1000
+        precio: 1000,
+        cantidad: 0
     },
 ]
