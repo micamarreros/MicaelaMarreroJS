63a1bb47cf9ae5141c9ede77de17912bfa5f3bee
Micaela Marrero <micamarrero@gmail.com>
2023-08-26 13:46:03 -0300

    Sigo trabajando en eliminar productos carrito

--- js/carrito.js
+++ js/carrito.js
@@ -1,33 +1,47 @@
 // Contenedor de todos los productos del carrito
 const contenedorCarrito = document.getElementById("contenedor-carrito");
 
-let productosEnCarrito = localStorage.getItem("productos-en-carrito");
-productosEnCarrito = JSON.parse(productosEnCarrito);
+// Traigo a carrito de local storage
+let productosEnCarrito = JSON.parse(localStorage.getItem("productos-en-carrito")) || [];
 
-productosEnCarrito.innerHTML = "";
+// Uso funcion mostrar carrito con productos traidos de local storage
+carrito(productosEnCarrito);
 
-productosEnCarrito.forEach(producto => {
+// Muestro el carrito con los productos traidos del local storage
+function carrito(productosEnCarrito) {
 
-    const div = document.createElement("div");
-    div.classList.add("carrito-producto");
-    div.innerHTML= `
-    <img class="carrito-producto-img" src="${producto.img}" alt="${producto.nombre}">
-    <div class="carrito-descripcion-productos">
-        <h5 class="carrito-producto-nombre">${producto.nombre}</h5>
-        <p>$ ${producto.precio}</p>
-        <p>${producto.cantidad}</p>
-        <p>Subtotal ${producto.precio * producto.cantidad}</p>
-   
-        <button class="btn-eliminar" onclick="eliminarDelCarrito(${producto.id})">Eliminar del carrito</button>
-    </div>
-`;
+    // Borro contenedor para que muestre los productos de nuevo
+    contenedorCarrito.innerHTML ="";
+
+    productosEnCarrito.forEach(producto => {
+
+        const div = document.createElement("div");
+        div.classList.add("carrito-producto");
+        div.innerHTML= `
+        <img class="carrito-producto-img" src="${producto.img}" alt="${producto.nombre}">
+        <div class="carrito-descripcion-productos">
+            <h5 class="carrito-producto-nombre">${producto.nombre}</h5>
+            <p>$ ${producto.precio}</p>
+            <p>${producto.cantidad}</p>
+            <p>Subtotal ${producto.precio * producto.cantidad}</p>
+       
+            <button class="btn-eliminar" onclick="eliminarDelCarrito(${producto.id})">Eliminar del carrito</button>
+        </div>
+    `;
+        
+    contenedorCarrito.append(div);
     
-contenedorCarrito.append(div);
+    })
+}
 
-})
+function eliminarDelCarrito(id) {
+    
+    // Obtengo el producto del array
+    const findProducto = productosEnCarrito.fin(e => e.id == id);
 
-function eliminarDelCarrito(index) {
-    carrito.splice(index, 1);
+    const index = productosEnCarrito.indexOf(findProducto);
+    
+    productosEnCarrito.splice(index, 1);
     
     Swal.fire({
         position: 'center',
@@ -37,8 +51,9 @@ function eliminarDelCarrito(index) {
         timer: 1500
     })
 
-    carrito();
+    // Muestro el carrito con los productos eliminados
+    carrito(productosEnCarrito);
     
-    // Actualizo y guardo en local storage los productos del carrito
-    localStorage.setItem("productos-en-carrito", JSON.stringify(carrito));
+    // Actualizo y guardo en local storage los productos del nuevo carrito
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
 }
